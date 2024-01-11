123f56d7467a907173472c8969fd9b312165f584
nika <nika.kasro@gmail.com>
2024-01-12 00:37:50 +0400

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,40 @@
+﻿using Singleton;
+
+Console.WriteLine("Testing Singleton Design Pattern\n");
+
+// method to create Singleton instance.
+static void CreateSingletonInstance()
+{
+    SingletonClass singletonInstance = SingletonClass.GetInstance();
+    Console.WriteLine($"Singleton instance ID: {SingletonClass.Id}");
+}
+
+
+
+Thread createInstanceForThread1 = new Thread(() =>
+{
+    CreateSingletonInstance();
+});
+
+Thread createInstanceForThread2 = new Thread(() =>
+{
+    CreateSingletonInstance();
+});
+
+
+// start execution of threads.
+createInstanceForThread1.Start();
+createInstanceForThread2.Start();
+
+// Instruct main thread to wait for the completion of threads before proceeding further.
+// The main thread will be blocked until threads finish their execution.
+createInstanceForThread1.Join();
+createInstanceForThread2.Join();
+
+Console.WriteLine(
+            "\n If Id's are the same, then singleton works fine"
+        );
+
+
+//booo
+Console.WriteLine(" \n _________.__               .__          __                 \r\n /   _____/|__| ____    ____ |  |   _____/  |_  ____   ____  \r\n \\_____  \\ |  |/    \\  / ___\\|  | _/ __ \\   __\\/  _ \\ /    \\ \r\n /        \\|  |   |  \\/ /_/  >  |_\\  ___/|  | (  <_> )   |  \\\r\n/_______  /|__|___|  /\\___  /|____/\\___  >__|  \\____/|___|  /\r\n        \\/         \\//_____/           \\/                 \\/ \r\n    .___            .__                                      \r\n  __| _/____   _____|__| ____   ____                         \r\n / __ |/ __ \\ /  ___/  |/ ___\\ /    \\                        \r\n/ /_/ \\  ___/ \\___ \\|  / /_/  >   |  \\                       \r\n\\____ |\\___  >____  >__\\___  /|___|  /                       \r\n     \\/    \\/     \\/  /_____/      \\/                        \r\n__________         __    __                                  \r\n\\______   \\_____ _/  |__/  |_  ___________  ____             \r\n |     ___/\\__  \\\\   __\\   __\\/ __ \\_  __ \\/    \\            \r\n |    |     / __ \\|  |  |  | \\  ___/|  | \\/   |  \\           \r\n |____|    (____  /__|  |__|  \\___  >__|  |___|  /           \r\n                \\/                \\/           \\/        ");
---
+++ Singleton/SingletonClass.cs
@@ -0,0 +1,69 @@
+﻿namespace Singleton;
+
+
+//
+// Summary:
+// This Singleton implementation uses
+// Techniques such as "double check lock" and the lock statement
+// To ensure that the creation of the instance is synchronized across multiple threads.
+// It is safe in multithreaded environment and provides lazy initialization for the
+// Singleton object.
+
+
+
+// By marking the class as sealed, we are preventing other classes from inheriting from it. 
+// In the context of a Singleton, inheritance is typically not desired, 
+// as it could lead to multiple instances of the class if a derived class attempts to create its own instance.
+public sealed class SingletonClass
+{
+    // Using a private static readonly instance to ensure thread safety.
+    private static readonly object _lock = new object();
+    private static SingletonClass? _instance;
+    public static string? Id { get; private set; }
+
+    #region Constructor
+
+    /// <summary> 
+    /// Constructor of Singleton type class must be private 
+    /// to prevent instantiation from outside.
+    /// </summary>
+    private SingletonClass()
+    {
+        Id = Guid.NewGuid().ToString(); //Only for testing purposes
+
+        // Initialization code, if needed.
+    }
+
+    #endregion
+
+    /// <summary>
+    /// <para> 
+    ///     Public method to get the instance of the class.
+    /// </para>
+    /// <para>
+    ///     To ensure for thread safety, we have to double-check locking for it. <br/>
+    ///     If there's no Singleton instance created yet, multiple threads can
+    ///     simultaneously try to pass it at the same time.<br/>
+    ///     We must ensure, that the first thread will lock and 
+    ///     proceed further. 
+    /// </para>
+    /// </summary>
+    public static SingletonClass GetInstance()
+    {
+        if (_instance == null)
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new SingletonClass();
+                    Console.WriteLine("Singleton instance has been created");
+                }
+            }
+
+        }
+
+        return _instance;
+    }
+}
+
