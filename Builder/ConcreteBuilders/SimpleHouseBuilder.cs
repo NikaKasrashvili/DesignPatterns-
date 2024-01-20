f6415ffb34491d527e1a0af7ea006544d18667fb
nika <nika.kasro@gmail.com>
2024-01-21 00:53:55 +0400

    Builder DP - add Concrete builder class, that implemetns builder interface

---
+++ Builder/ConcreteBuilders/SimpleHouseBuilder.cs
@@ -0,0 +1,56 @@
+﻿namespace Builder.ConcreteBuilders;
+
+/// <summary>
+/// Concrete builder class, that follows builder interface.
+/// </summary>
+internal class SimpleHouseBuilder : IHouseBuilder
+{
+    private House _house = new House();
+
+    #region constructor
+    public SimpleHouseBuilder()
+    {
+        Reset();
+    }
+    public void Reset()
+    {
+        _house = new House();
+    }
+    #endregion
+
+    public void BuildBalcony()
+    {
+        _house.Add("Balcony has been added");
+    }
+
+    public void SetFlooringType(string flooringType)
+    {
+        _house.Add(flooringType);
+    }
+
+    public void SetRoofType(string roofType)
+    {
+        _house.Add(roofType);
+    }
+
+    public void SetRooms(int rooms)
+    {
+        _house.Add(rooms.ToString());
+    }
+
+    public void SetType(string type)
+    {
+        _house.Add(type);
+    }
+
+    /// <summary>
+    /// method for retrieving made product.
+    /// </summary>
+    /// <returns>created object(product) of <see cref="House"/> class</returns>
+    public House BuildHouse()
+    {
+        House result = _house;
+        Reset();
+        return result;
+    }
+}
--- Builder/IHouseBuilder.cs
+++ Builder/IHouseBuilder.cs
@@ -6,10 +6,29 @@
 /// </summary>
 internal interface IHouseBuilder
 {
+    /// <summary>
+    /// Sets the type of House. F.ex."wooden", "victorian", "farmhouse"...
+    /// </summary>
+    /// <param name="type"></param>
     void SetType(string type);                      //build partA 
+    /// <summary>
+   /// Defines how many rooms should house have.
+   /// </summary>
+   /// <param name="rooms"></param>
     void SetRooms(int rooms);                       //build partB 
+    /// <summary>
+    /// Sets the type of room - "flat", "gable", "mansard"...
+    /// </summary>
+    /// <param name="roofType"></param>
     void SetRoofType(string roofType);              //build partC 
+    /// <summary>
+    /// Defines flooring type - "laminate", "plank", "solid hardwood"...
+    /// </summary>
+    /// <param name="flooringType"></param>
     void SetFlooringType(string flooringType);      //build partD 
+    /// <summary>
+    /// Defines whether or not house have balcony.
+    /// </summary>
     void BuildBalcony ();                           //build partE
 }
  
