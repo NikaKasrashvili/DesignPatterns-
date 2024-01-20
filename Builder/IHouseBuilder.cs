2e593692b314f6b553c3f703ac2845271ef0eaa5
nika <nika.kasro@gmail.com>
2024-01-21 00:29:51 +0400

    Builder DP: define the builder interface for house

---
+++ Builder/IHouseBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace Builder;
+
+/// <summary>
+/// Interface of builder dp, that defines methods
+/// for creating the different parts of the Product objects.
+/// </summary>
+internal interface IHouseBuilder
+{
+    void SetType(string type);                      //build partA 
+    void SetRooms(int rooms);                       //build partB 
+    void SetRoofType(string roofType);              //build partC 
+    void SetFlooringType(string flooringType);      //build partD 
+    void BuildBalcony ();                           //build partE
+}
+ 
