8a425a22b082a332fec1b6692401fe11a9d15b9d
nika <nika.kasro@gmail.com>
2024-01-20 23:22:35 +0400

    Builder DP: add house class (PRODUCT).

---
+++ Builder/House.cs
@@ -0,0 +1,29 @@
+﻿namespace Builder;
+
+
+/// <summary>
+/// Final product, that we want to build.
+/// </summary>
+internal class House
+{
+    private List<object> _parts = new List<object>();
+
+    public void Add(string part)
+    {
+        _parts.Add(part);
+    }
+
+    public string ListParts()
+    {
+        string str = string.Empty;
+
+        for (int i = 0; i < _parts.Count; i++)
+        {
+            str += _parts[i] + ", ";
+        }
+
+        str = str.Remove(str.Length - 2); 
+
+        return "House parts: " + str + "\n";
+    }
+}
