6f87c34d35b8616ce6ef5b0ac1182baa53ec5cb6
nika <nika.kasro@gmail.com>
2024-01-25 19:58:34 +0400

    Adapter: add adaptee class and interface for adapting

---
+++ Adapter/INewSystem.cs
@@ -0,0 +1,12 @@
+﻿namespace Adapter;
+
+/// <summary>
+/// Interface that declares methods of new system.
+/// </summary>
+internal interface INewSystem
+{
+    /// <summary>
+    /// Request of <see cref="INewSystem"/> interface.
+    /// </summary>
+    void NewRequest();
+}
---
+++ Adapter/OldSystem.cs
@@ -0,0 +1,16 @@
+﻿namespace Adapter;
+
+/// <summary>
+///  Existing system with an incompatible interface. <br/>
+///  It's called <strong>  "Adaptee". </strong>
+/// </summary>
+internal class OldSystem
+{
+    /// <summary>
+    /// "Request" method from <see cref="OldSystem"/> class.
+    /// </summary>
+    public void Request()
+    {
+        Console.WriteLine("Old System Request");
+    }
+}
