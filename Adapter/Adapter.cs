b506cb22d94c720f085602c089631e4b8ab1a76b
nika <nika.kasro@gmail.com>
2024-01-26 23:32:54 +0400

    Adapter: add adapter and new system classes

---
+++ Adapter/Adapter.cs
@@ -0,0 +1,21 @@
+﻿namespace Adapter;
+
+/// <summary>
+/// <strong>Adapter</strong> class that makes <see cref="OldSystem"/> compatible with <see cref="INewSystem"/>.
+/// </summary>
+internal class Adapter : INewSystem
+{
+    private OldSystem? _oldSystem;
+
+    public Adapter(OldSystem oldSystem)
+    {
+        _oldSystem = oldSystem;
+    }
+
+    //new system method interface implementation
+    public void NewRequest()
+    {
+        _oldSystem?.Request();
+    }
+  
+}
---
+++ Adapter/NewSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter;
+
+internal class NewSystem
+{
+    private INewSystem _newSystem;
+
+    public NewSystem(INewSystem newSystem)
+    {
+        _newSystem= newSystem;
+    }
+
+    public void PerformNewRequest()
+    {
+        _newSystem.NewRequest();
+    }
+}
