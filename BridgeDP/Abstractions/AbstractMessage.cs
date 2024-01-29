1f536a12959449db03e65b15e7bd02ea6d66f91c
nika <nika.kasro@gmail.com>
2024-01-29 19:44:08 +0400

    Abstraction Dp: add abstractions

---
+++ BridgeDP/Abstractions/AbstractMessage.cs
@@ -0,0 +1,27 @@
+﻿using BridgeDP.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeDP.Abstractions;
+
+/// <summary>
+/// Abstraction class: defines the interface
+/// </summary>
+internal abstract class AbstractMessage
+{
+    protected IMessageSender? _sender;
+
+    public AbstractMessage(IMessageSender sender)
+    {
+        _sender = sender;
+    }
+
+    /// <summary>
+    /// abstract method to send a text.
+    /// </summary>
+    /// <param name="text"></param>
+    public abstract void Send(string text);
+}
---
+++ BridgeDP/Abstractions/ExtendedMessages.cs
@@ -0,0 +1,14 @@
+﻿using BridgeDP.Implementations;
+
+namespace BridgeDP.Abstractions;
+
+internal class ExtendedMessages : AbstractMessage
+{
+    public ExtendedMessages(IMessageSender sender) : base(sender)
+    { }
+
+    public override void Send(string text)
+    {
+        _sender?.SendMessage(text);
+    }
+}
