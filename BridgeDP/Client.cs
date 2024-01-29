95ac09d06673fb24b69c5de60d756728935de33c
nika <nika.kasro@gmail.com>
2024-01-29 19:44:45 +0400

    Abstraction Dp: add client code and program.cs

---
+++ BridgeDP/Client.cs
@@ -0,0 +1,14 @@
+﻿using BridgeDP.Abstractions;
+
+namespace BridgeDP;
+
+/// <summary>
+/// client code should only depend on the Abstraction class.
+/// </summary>
+internal class Client
+{
+    public void ClientCode(AbstractMessage abstraction, string text)
+    {
+        abstraction.Send(text);
+    }
+}
---
+++ BridgeDP/Program.cs
@@ -0,0 +1,25 @@
+﻿using BridgeDP;
+using BridgeDP.Abstractions;
+using BridgeDP.Implementations;
+
+Console.WriteLine("Testing Bridge design pattern.\n");
+
+Client client = new Client();
+
+// Create instances of concrete implementations: EmailSender and SmsSender
+IMessageSender emailSender = new EmailSender();
+IMessageSender smsSender = new SmsSender();
+
+AbstractMessage abstraction;
+
+// send text via email.
+abstraction = new ExtendedMessages(emailSender);    
+client.ClientCode(abstraction, "That's one small step for man, one giant leap for mankind.");
+
+
+// send text via sms.
+abstraction = new ExtendedMessages(smsSender);
+client.ClientCode(abstraction, "Booo!\n");
+
+
+
