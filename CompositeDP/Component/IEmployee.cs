183d176cfe111bf9d1128a90218dd1131ecdadc5
nika <nika.kasro@gmail.com>
2024-01-30 23:29:48 +0400

    Revert "Revert "Composite: add component interface""
    This reverts commit b74098c59a4dfa4a11a53f04d27121ba35e21b13.

---
+++ CompositeDP/Component/IEmployee.cs
@@ -0,0 +1,13 @@
+﻿namespace CompositeDP.Component;
+
+/// <summary>
+/// <strong> Component   </strong> interface that defines the common operations for individual employees and departments.
+/// </summary>
+internal interface IEmployee
+{
+    /// <summary>
+    /// Method declared in <see cref="IEmployee"/> component interface.<br/>
+    /// It's common for both simple and complex objects.
+    /// </summary>
+    void DisplayDetails();
+}
