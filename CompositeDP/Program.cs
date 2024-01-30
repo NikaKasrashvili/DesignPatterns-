711917241fbab1ae90a490b2e2479653178f48f6
nika <nika.kasro@gmail.com>
2024-01-31 00:16:51 +0400

    Composite: add some changes

--- CompositeDP/Program.cs
+++ CompositeDP/Program.cs
@@ -2,7 +2,7 @@
 using CompositeDP.Composite;
 using CompositeDP.Leaf;
 
-Console.WriteLine("Testing Composite design pattern.");
+Console.WriteLine("Testing Composite design pattern.\n");
 
 // Step 1:
 // Creating individual employees
@@ -21,7 +21,7 @@ developmentDepartment.AddEmployee(employee2);
 // adding sub-department into development department
 
 IEmployee employee3 = new IndividualEmployee("Vano", 29, "Project manager");
-IEmployee employee4 = new IndividualEmployee("Acho", 25, "AI team");
+IEmployee employee4 = new IndividualEmployee("Acho", 25, "AI developer");
 
 var ProjectManagementDepartment = new Department("PM team department");
 ProjectManagementDepartment.AddEmployee(employee3);
@@ -32,10 +32,18 @@ subDepartment.AddEmployee(employee4);
 // Adding the sub-department to the development department
 developmentDepartment.AddEmployee(subDepartment);
 
+
+// Final step: Creating COmpany!
 // Creating a company department and adding the development and PM departments to it
 var company = new Department("Company");
 company.AddEmployee(developmentDepartment);
 company.AddEmployee(ProjectManagementDepartment);
 
-// Displaying details of the company department, which recursively displays details of all employees and sub-departments
+// Displaying details of the company department,
+// which recursively displays details of all employees and sub-departments
+
 company.DisplayDetails();
+
+
+
+Console.WriteLine("_________                                    .__  __          \r\n\\_   ___ \\  ____   _____ ______   ____  _____|__|/  |_  ____  \r\n/    \\  \\/ /  _ \\ /     \\\\____ \\ /  _ \\/  ___/  \\   __\\/ __ \\ \r\n\\     \\___(  <_> )  Y Y  \\  |_> >  <_> )___ \\|  ||  | \\  ___/ \r\n \\______  /\\____/|__|_|  /   __/ \\____/____  >__||__|  \\___  >\r\n        \\/             \\/|__|              \\/              \\/");
