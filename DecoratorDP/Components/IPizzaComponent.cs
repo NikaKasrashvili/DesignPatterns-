02ba1fa1aabc98eb38c5f1161d185db1c724561d
nika <nika.kasro@gmail.com>
2024-02-02 23:00:03 +0400

    Decorator: add component (with it's interface)

---
+++ DecoratorDP/Components/IPizzaComponent.cs
@@ -0,0 +1,19 @@
+﻿namespace DecoratorDP.Components;
+
+/// <summary>
+/// <strong>Component</strong> Interface : 
+/// defines operations that can be altered by decorators.
+/// </summary>
+internal interface IPizzaComponent
+{
+    /// <summary>
+    /// Method to get description.
+    /// </summary>
+    /// <returns></returns>
+    string GetDescription();
+    /// <summary>
+    /// Method to get cost of additional decorators and base product.
+    /// </summary>
+    /// <returns></returns>
+    double GetCost();
+}
---
+++ DecoratorDP/Components/PizzaComponent.cs
@@ -0,0 +1,18 @@
+﻿namespace DecoratorDP.Components;
+
+/// <summary>
+/// Concrete Component: default implementation of <see cref="IPizzaComponent"/> methods.
+/// </summary>
+internal class PizzaComponent : IPizzaComponent
+{
+   public string GetDescription()
+    {
+        return "Plain pizza";
+    }
+
+    public double GetCost()
+    {
+        return 5.0; // Base cost for plain pizza
+    }
+}
+
